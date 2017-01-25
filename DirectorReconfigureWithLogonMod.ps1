fcfb30651484c707bbdfcb4e318210ca49878b77
John McBride <john@johnmcbride.me>
2017-01-24 20:59:58 -0500

    Added disclaimer

--- DirectorReconfigureWithLogonMod.ps1
+++ DirectorReconfigureWithLogonMod.ps1
@@ -1,3 +1,10 @@
+# /*************************************************************************
+# *
+# * THIS SAMPLE CODE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# *
+# *************************************************************************/
 <#
 Script to configure director after an upgrade or just to make sure that all servers are 
 configured the same. Also a great way to document the settings you want in your environment.
